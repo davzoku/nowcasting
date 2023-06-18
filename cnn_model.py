b897476b8a3f9aa9734ef440eeff9654bf89acd6
Walter Teng <16046667+davzoku@users.noreply.github.com>
2023-06-18 13:27:55 +0800

    add mvp

--- cnn_model.py
+++ cnn_model.py
@@ -3,32 +3,81 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from tqdm import tqdm
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import matplotlib.pyplot as plt
 
 
-class CNN_1d(nn.Module):
-    def __init__(self, n_features, lookback):
+class CNN_1D(nn.Module):
+    def __init__(self, n_features, lookback, layers="single"):
         super().__init__()
-        self.conv1 = nn.Conv1d(in_channels=n_features,
-                               out_channels=64,
-                               kernel_size=3,
-                               padding=1,
-                               stride=1)
-        self.pool = nn.MaxPool2d(kernel_size=2)
-        self.fc1 = nn.Linear(8 * 12, 256)
-        self.fc2 = nn.Linear(256, 1)
+        self.layers = layers
+        
+        if self.layers == "single":
+            self.conv1 = nn.Conv1d(in_channels=n_features,
+                                   out_channels=64,
+                                   kernel_size=3,
+                                   padding=1,
+                                   stride=1)
+            self.pool = nn.MaxPool1d(kernel_size=2)
+            self.fc1 = nn.Linear(64 * (lookback // 2), 256)
+            self.fc2 = nn.Linear(256, 1)
+        else:
+            # # self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=64, kernel_size=3, padding=1, stride=1)
+            # # self.conv2 = nn.Conv1d(in_channels=64, out_channels=128, kernel_size=3, padding=1, stride=1)
+            # # self.conv3 = nn.Conv1d(in_channels=128, out_channels=256, kernel_size=3, padding=1, stride=1)
+            # self.conv1 = nn.Conv1d(in_channels=n_features,out_channels = 64, kernel_size = 3, padding =1, stride =1)
+            # self.conv2 = nn.Conv1d(in_channels=64,out_channels = 128, kernel_size = 3,padding=1, stride=1)
+            # # self.conv3 = nn.Conv1d(in_channels=128,out_channels = 256, kernel_size = 5, padding = 2)            
+            # self.pool = nn.MaxPool1d(kernel_size=3) 
+            # self.fc1 = nn.Linear(128 * (lookback // 2), 1536)
+            # self.fc2 = nn.Linear(1536, 1)
+            self.conv0 = nn.Conv1d(in_channels=n_features,out_channels = 3, kernel_size = 1)
+            self.conv1 = nn.Conv1d(in_channels=n_features,out_channels = 3, kernel_size = 3,padding=1)
+            self.conv2 = nn.Conv1d(in_channels=n_features,out_channels = 3, kernel_size = 5, padding = 2)
+            self.maxpool1 = nn.MaxPool1d(kernel_size = 3,dilation=1)
+            self.linear = nn.Linear(in_features=lookback*3,out_features=1)        
         self.relu = nn.ReLU()
         self.n_features = n_features
         self.lookback = lookback
-        # self.device = self.set_device()
         self.device = 'cpu'
 
     def forward(self, x):
-        # x = x.to(self.device)
-        x = self.relu(self.conv1(x))
-        x = self.pool(x)
-        x = x.view(-1, 8 * 12)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        if self.layers == "single":
+            x = self.relu(self.conv1(x))
+            x = self.pool(x)
+            x = x.view(-1, 64 * (self.lookback // 2))
+            x = self.relu(self.fc1(x))
+            x = self.fc2(x)     
+        else:
+            # # # x = x.permute(0,2,1)
+            # # x1 = self.conv1(x)
+            # # x2 = self.conv2(x)
+            # # x3 = self.conv3(x)
+            # # # x = torch.cat((x1,x2,x3),dim=1)
+            # # x = self.pool(x)
+            # # x = torch.flatten(x,1)
+            # # # x = self.relu(self.fc1(x))
+            # # x = self.fc2(x) 
+            # x = self.relu(self.conv1(x))
+            # x = self.pool(x)
+            # x = self.relu(self.conv2(x))
+            # x = self.pool(x)
+            # # x = self.relu(self.conv3(x))
+            # # x = self.pool(x)
+            # x = x.view(-1, 128 * (self.lookback // 2))
+            # x = self.relu(self.fc1(x))
+            # x = self.fc2(x)              
+            # x = x.permute(0,2,1)
+            x0 = self.conv0(x)
+            x1 = self.conv1(x)
+            x2 = self.conv2(x)
+            x = torch.cat((x0,x1,x2),dim=1)
+            x = self.maxpool1(x)
+            x = torch.flatten(x,1)
+            x = self.relu(x)
+            x = self.linear(x)
+            # x = self.dropout(x)
+            x = self.relu(x)            
         return x
 
     def fit(self,
@@ -42,6 +91,8 @@ class CNN_1d(nn.Module):
         self.train()
         best_val_loss = np.Inf
         counter = 0
+        train_losses = [] 
+        val_losses = []
 
         for epoch in tqdm(range(num_epochs)):
             for (x_batch, y_batch) in train_loader:
@@ -54,10 +105,10 @@ class CNN_1d(nn.Module):
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
+            
+            print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}', end=" ")
 
-            print('Epoch {}/{}, Loss: {:.4f},'.format(epoch + 1, num_epochs,
-                                                      loss.item()),
-                  end=" ")
+            train_losses.append(loss.item())
 
             # Compute validation loss
             with torch.no_grad():
@@ -71,7 +122,8 @@ class CNN_1d(nn.Module):
                     val_loss += criterion(self.forward(x_batch), y_batch)
                 val_loss /= len(val_loader)
 
-                print('Val loss:{:.4f}'.format(val_loss))
+                print(f'Val loss:{val_loss:.4f}')
+                val_losses.append(val_loss.item())
 
 
                 # Check for improvement
@@ -83,31 +135,55 @@ class CNN_1d(nn.Module):
                     if counter >= patience:
                         print(f'Early stopping at epoch {epoch+1}')
                         break
+        # Plot the training loss
+        plt.plot(range(1, epoch + 2), train_losses, label='Training Loss')
+        # plt.plot(range(1, epoch + 2), val_losses, label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Training Loss')
+        plt.title('Training Loss')
+        plt.show()
 
-    def evaluate(self, test_loader, criterion):
+    def evaluate(self, test_loader, criterion, scaler):
         self.eval()
         with torch.no_grad():
             total_loss = 0
+            total_mse = 0
+            total_rmse = 0
+            total_mae = 0
+            num_samples = 0
+
             for (x_batch, y_batch) in test_loader:
-                # Forward pass
-                # y_pred = model.forward(x_batch.transpose(1, -2))
                 x_batch = x_batch.reshape(-1, self.n_features, self.lookback)
                 x_batch = x_batch.to(self.device)
                 y_batch = y_batch.to(self.device)
                 y_pred = self.forward(x_batch)
+                print(f"what is y_batch {type(y_batch)}, {y_batch.shape}")
+                print(f"what is y_pred {type(y_pred)}, {y_pred.shape}")
 
                 # Compute loss
-                # criterion = nn.MSELoss()
                 loss = criterion(y_pred, y_batch)
-                print(loss)
-                # Accumulate loss
-                total_loss += loss.item()
 
-            # Compute average loss
-            avg_loss = total_loss / len(test_loader)
-            print(f"Total Loss: {total_loss}")
-            print(f"len(test_loader): {len(test_loader)}")
-            print(f"Test Loss: {avg_loss:.4f}")
+                # Compute RMSE and MAE in the original units
+                y_pred_orig = scaler.inverse_transform(y_pred.reshape(-1, 1))
+                y_batch_orig = scaler.inverse_transform(y_batch.reshape(-1, 1))
+                mse = mean_squared_error(y_batch_orig, y_pred_orig)
+                rmse = mean_squared_error(y_batch_orig, y_pred_orig, squared = False)
+                mae = mean_absolute_error(y_batch_orig, y_pred_orig)
+
+                # Accumulate loss, RMSE, and MAE
+                total_loss += loss.item() * x_batch.size(0)
+                total_mse += mse * x_batch.size(0)
+                total_rmse += rmse * x_batch.size(0)
+                total_mae += mae * x_batch.size(0)
+                num_samples += x_batch.size(0)
+
+            # Compute average loss, RMSE, and MAE
+            avg_loss = total_loss / num_samples
+            avg_mse = total_mse / num_samples
+            avg_rmse = total_rmse / num_samples
+            avg_mae = total_mae / num_samples
+
+            print(f"Test Loss: {avg_loss:.4f}, MSE: {avg_mse:.4f}, RMSE: {avg_rmse:.4f}, MAE: {avg_mae:.4f}")
 
     def predict(self, data):
         # inputs = data.to(self.device)
@@ -125,5 +201,5 @@ class CNN_1d(nn.Module):
             device = torch.device("mps")
         else:
             device = torch.device('cpu')
-        print("torch.device =>", device)
+        print(f"torch.device => {device}")
         return device
--- windowing.py
+++ windowing.py
@@ -3,60 +3,205 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 
 
+# class WindowGenerator:
+#     def __init__(self, data, lookback, lookahead, batch_size, label_idx):
+#         self.lookback = lookback
+#         self.lookahead = lookahead
+#         self.batch_size = batch_size
+#         self.data = data
+#         self.label_idx = label_idx
+#         self.X = []
+#         self.y = []
+
+#     def split_window(self):
+#         # <-- lookback --> <----- i -----> <-- lookahead -->
+#         # |---------------- data length -------------------|
+
+#         # start at lookback, end before lookahead
+#         idx_start = self.lookback
+#         idx_end = len(self.data) - self.lookahead
+
+#         for i in range(idx_start, idx_end):
+#             # append to X the last n rows for lookback,
+#             # and all columns as features, including the
+#             # ts data (label) itself
+#             self.X.append(self.data[i - self.lookback : i, :])
+
+#             # append to y the lookahead row we want to predict,
+#             # and only the label column
+#             self.y.append(self.data[i + self.lookahead, self.label_idx])
+
+#         return (
+#             torch.Tensor(np.array(self.X)),
+#             torch.Tensor(np.array(self.y).reshape(-1, 1)),
+#         )
+
+#     def make_dataloader(self):
+#         X, y = self.split_window()
+#         dataset = TSDataset(X, y)
+
+#         # dataloader = DataLoader(dataset,
+#         #                         batch_size=self.batch_size,
+#         #                         shuffle=True)
+#         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+#         return dataloader
+
+
+# class TSDataset(Dataset):
+#     def __init__(self, X, y):
+#         self.X = X
+#         self.y = y
+
+#     def __len__(self):
+#         return len(self.y)
+
+#     def __getitem__(self, idx):
+#         features = self.X[idx]
+#         label = self.y[idx]
+
+#         return features, label
+
+# xxxxxxx
+
 class WindowGenerator:
-    def __init__(self, data, lookback, lookahead, batch_size, label_idx):
+    def __init__(
+        self, dataset, target, lookback=6, lookahead=1, batch_size=18, multi_step=False
+    ):
+        self.target = target.ravel().astype(np.float32)
+        self.dataset = np.column_stack((dataset, target)).astype(np.float32)
+        
         self.lookback = lookback
         self.lookahead = lookahead
         self.batch_size = batch_size
-        self.data = data
-        self.label_idx = label_idx
-        self.X = []
-        self.y = []
+        self.multi_step = multi_step
+        self.input = []
+        self.output = []
 
-    def split_window(self):
+       
+    def make_dataloader(self):
         # <-- lookback --> <----- i -----> <-- lookahead -->
         # |---------------- data length -------------------|
 
-        # start at lookback, end before lookahead
-        idx_start = self.lookback
-        idx_end = len(self.data) - self.lookahead
+        # start at lookback, end before lookahead        
+        X, y = self.split_data()
+        dataset = TimeSeriesDataset(X, y)
 
-        for i in range(idx_start, idx_end):
-            # append to X the last n rows for lookback,
-            # and all columns as features, including the
-            # ts data (label) itself
-            self.X.append(self.data[i - self.lookback : i, :])
-
-            # append to y the lookahead row we want to predict,
-            # and only the label column
-            self.y.append(self.data[i + self.lookahead, self.label_idx])
-
-        return (
-            torch.Tensor(np.array(self.X)),
-            torch.Tensor(np.array(self.y).reshape(-1, 1)),
-        )
-
-    def make_dataloader(self):
-        X, y = self.split_window()
-        dataset = TSDataset(X, y)
-
-        # dataloader = DataLoader(dataset,
-        #                         batch_size=self.batch_size,
-        #                         shuffle=True)
         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
         return dataloader
 
+    
+    def split_data(self):
+        for i in range(len(self.dataset) - self.lookahead - self.lookback):
+            self.input.append(self.dataset[i : i + self.lookback])
+            if self.multi_step == True:
+                self.output.append(self.target[i + self.lookback : i + self.lookback + self.lookahead])
+            else:
+                self.output.append(self.target[i + self.lookback + self.lookahead])
 
-class TSDataset(Dataset):
+        # if self.batch_size:
+        #     self.input, self.output = self._batch_generator(self.input, self.output, self.batch_size)
+        #     return self.input, self.output
+        return np.array(self.input), np.array(self.output)
+
+    # def _batch_generator(self, X, y, batch_size) -> tuple:
+    #     # doesn't split if batch size is greater than the size
+    #     if batch_size is None or batch_size >= X.shape[0]:
+    #         return [X], [y]
+    #     else:
+    #         # using divmod to find the sections and remainder
+    #         sections, remainder = divmod(X.shape[0], batch_size)
+    #         # creates a tuple of list of arrays
+    #         # the last array is the remainder which is appeneded to the list
+    #         input, output = (
+    #             np.split(X[:-remainder], sections),
+    #             np.split(y[:-remainder], sections),
+    #         )
+    #         input.append(X[-remainder:])
+    #         output.append(y[-remainder:])
+    #         # returns a tuple of list of arrays
+    #         return input, output
+
+
+        
+class TimeSeriesDataset(Dataset):
     def __init__(self, X, y):
         self.X = X
         self.y = y
 
     def __len__(self):
-        return len(self.y)
+        return len(self.X)
 
     def __getitem__(self, idx):
-        features = self.X[idx]
-        label = self.y[idx]
+        return self.X[idx], self.y[idx]
 
-        return features, label
+
+# import numpy as np
+
+
+# class WindowGenerator:
+#     def __init__(
+#         self, dataset, target, test_size=0.2, lookback=6, lookahead=1, batch_size=32, multi_step=False
+#     ):
+#         self.target = target.ravel().astype(np.float32)
+#         self.dataset = np.column_stack((dataset, target)).astype(np.float32)
+        
+#         self.lookback = lookback
+#         self.lookahead = lookahead
+#         self.batch_size = batch_size
+#         self.test_size = test_size
+#         self.multi_step = multi_step
+#         self.input = []
+#         self.output = []
+
+#     def split_data(self):
+#         for i in range(len(self.dataset) - self.lookahead - self.lookback):
+#             self.input.append(self.dataset[i : i + self.lookback])
+#             if self.multi_step == True:
+#                 self.output.append(self.target[i + self.lookback : i + self.lookback + self.lookahead])
+#             else:
+#                 self.output.append(self.target[i + self.lookback + self.lookahead])
+
+#         if self.batch_size:
+#             self.input, self.output = self._batch_generator(self.input, self.output, self.batch_size)
+#             return self.input, self.output
+#         return np.array(self.input), np.array(self.output)
+
+#         # Initialy used train test split here
+#         # self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = self.test_size, random_state = 42)
+        
+
+#     def _batch_generator(self, X, y, batch_size) -> tuple:
+#         """
+#         Generate batches of data
+#         If batch size return the data back packaged in a list
+
+#         Parameters
+#         ----------
+#         X : np.ndarray
+#             Dataset
+#         y : np.ndarray
+#             Target array of labels
+#         batch_size : int, optional
+#             Size of batch, by default None
+
+#         Returns
+#         -------
+#         tuple
+#             Tuple of list of arrays in the form of inputs, outputs
+#         """
+#         # doesn't split if batch size is greater than the size
+#         if batch_size is None or batch_size >= X.shape[0]:
+#             return [X], [y]
+#         else:
+#             # using divmod to find the sections and remainder
+#             sections, remainder = divmod(X.shape[0], batch_size)
+#             # creates a tuple of list of arrays
+#             # the last array is the remainder which is appeneded to the list
+#             input, output = (
+#                 np.split(X[:-remainder], sections),
+#                 np.split(y[:-remainder], sections),
+#             )
+#             input.append(X[-remainder:])
+#             output.append(y[-remainder:])
+#             # returns a tuple of list of arrays
+#             return input, output
